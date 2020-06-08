06b59c994ce7bc16ec5e8c7d62bd3a8c66c59b55
ACWu <anthonycwu@yahoo.com>
2020-06-08 00:24:43 -0400

    Commit1

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+   inherit (nixpkgs) pkgs;
+   inherit (pkgs) haskellPackages;
+
+   haskellDeps = ps: with ps; [
+      base
+      lens
+      mtl
+      pdf-toolbox
+   ];
+
+   ghc = haskellPackages.ghcWithPackages haskellDeps;
+
+   nixPackages = [
+      ghc
+      pkgs.gdb
+      haskellPackages.cabal-install
+   ];
+in
+pkgs.stdenv.mkDerivation {
+   name = "env";
+   buildInputs = nixPackages;
+}
+
+
